e131dc3a2a058fa62bc98de4caa310dbcc9b2190
Kyle Snow <k1snow@bridgew.edu>
2020-09-29 13:16:25 -0400

    added several mood specific functions and variables

--- emotive_eliza.py
+++ emotive_eliza.py
@@ -26,6 +26,10 @@ class Eliza:
                     "sorry", "dream", "hello", "hi ", "maybe", "no", "your", "always", "think", "alike", "yes",
                     "friend", "computer"]
 
+        self.mood_boosters = ["rubber duck", "funk", "computers", "akira"]
+
+        self.mood_dampers = ["mayonnaise", "cardi b", "roommates"]
+
 
     def eliza(self):
         print("Hello, I am Eliza. Tell me your problems.")
@@ -33,6 +37,7 @@ class Eliza:
             sentence = input("> ")
             sentence = self.preprocess(sentence)
             # print("pre-process: " + sentence)
+            self.mood_keywords(sentence)
             sentence = self.keywords(sentence)
             # print("keyword: " + sentence)
             sentence = self.conjugate(sentence)
@@ -77,171 +82,208 @@ class Eliza:
         new_sentence = new_sentence
         return self.list_to_string(new_sentence)
 
-    def getreply(self, rep_num: int):
-        reply_list = []
+    def mood_keywords(self, sentence:str):
+        for word in self.mood_boosters:
+            if word in sentence:
+                self.set_mood(1)
+        for word in self.mood_dampers:
+            if word in sentence:
+                self.set_mood(-1)
+
+
+    def get_mood(self):
+        return self.mood
+
+    def set_mood(self, modifier:int):
+        self.mood += modifier
+
+    def getreply(self, rep_num: int, mood:int):
+        neutral_reply_list = []
+        positive_reply_list = ["I diagnose you with coolguy syndrome"]
+        negative_reply_list = ["I think it's time to end our session...", "You do realize that I'm paid by the hour, correct?"]
         reply = ""
 
+        def helper(mood):
+            if mood == 0:
+                reply = random.choice(neutral_reply_list)
+            elif mood > 0:
+                reply = random.choice(positive_reply_list)
+            else:
+                reply = random.choice(negative_reply_list)
+            return reply
+
         if rep_num == -1:
-            reply_list = ["What does that suggest to you?", "I see.", "I'm not sure I understand you fully.",
+            neutral_reply_list = ["What does that suggest to you?", "I see.", "I'm not sure I understand you fully.",
                           "Come, come, elucidate your thoughts.", "Can you elaborate on that?",
                           "That is quite interesting."]
-            reply = random.choice(reply_list)
+            positive_reply_list = ["You are fascinating.", "You're on a roll. Go on.", "Alright, I see"]
+            negative_reply_list = ["If you're not going to make any sense, why talk?"]
+            reply = helper(mood)
         elif rep_num == 0:
-            reply_list = ["Don't you believe that I can *", "Perhaps you would like me to be able to *",
+            neutral_reply_list = ["Don't you believe that I can *", "Perhaps you would like me to be able to *",
                           "You want me to be able to *"]
-            reply = random.choice(reply_list)
+            positive_reply_list = ["I think it's great that you think I can *"]
+            negative_reply_list = ["Why do you think I should have to *"]
+            reply = helper(mood)
         elif rep_num == 1:
-            reply_list = ["Perhaps you don't want to *", "Do you want to be able to *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Perhaps you don't want to *", "Do you want to be able to *"]
+            positive_reply_list = ["You must aspire to *"]
+            negative_reply_list = ["Why would you bother to *"]
+            reply = helper(mood)
         elif rep_num == 2:
-            reply_list = ["What makes you think I am *", "Does it please you to believe I am *",
+            neutral_reply_list = ["What makes you think I am *", "Does it please you to believe I am *",
                           "Perhaps you would like to be *", "Do you sometimes wish you were *"]
-            reply = random.choice(reply_list)
+            positive_reply_list = ["If it makes you happy, you can assume I am *"]
+            negative_reply_list = ["would you stop whining if I said I were *"]
+            reply = helper(mood)
         elif rep_num == 3:
-            reply_list = ["What makes you think I am *", "Does it please you to believe I am *",
+            neutral_reply_list = ["What makes you think I am *", "Does it please you to believe I am *",
                           "Perhaps you would like to be *", "Do you sometimes wish you were *"]
-            reply = random.choice(reply_list)
+            positive_reply_list = ["If it makes you happy, you can assume I am *"]
+            negative_reply_list = ["would you stop whining if I said I were *"]
+            reply = helper(mood)
         elif rep_num == 4:
-            reply_list = ["Don't you really *", "Why don't you *", "Do you wish to be able to *",
+            neutral_reply_list = ["Don't you really *", "Why don't you *", "Do you wish to be able to *",
                           "Does that trouble you?"]
-            reply = random.choice(reply_list)
+            #positive_reply_list = []
+            #negative_reply_list = []
+            reply = helper(mood)
         elif rep_num == 5:
-            reply_list = ["Tell me more about such feelings.", "Do you often feel *", "Do you enjoy feeling *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Tell me more about such feelings.", "Do you often feel *", "Do you enjoy feeling *"]
+            reply = helper(mood)
         elif rep_num == 6:
-            reply_list = ["Do you really believe I don't *", "Perhaps in good time I will *", "Do you want me to *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Do you really believe I don't *", "Perhaps in good time I will *", "Do you want me to *"]
+            reply = helper(mood)
         elif rep_num == 7:
-            reply_list = ["Do you think you should be able to *", "Why can't you *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Do you think you should be able to *", "Why can't you *"]
+            reply = helper(mood)
         elif rep_num == 8:
-            reply_list = ["Why are you interested in whether or not I am *", "Would you prefer if I were not *",
+            neutral_reply_list = ["Why are you interested in whether or not I am *", "Would you prefer if I were not *",
                           "Perhaps in your fantasies I am *"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 9:
-            reply_list = ["How do you know you can't *", "Have you tried?", "Perhaps you can now *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["How do you know you can't *", "Have you tried?", "Perhaps you can now *"]
+            reply = helper(mood)
         elif rep_num == 10:
-            reply_list = ["Did you come to me because you are *", "How long have you been *",
+            neutral_reply_list = ["Did you come to me because you are *", "How long have you been *",
                           "Do you believe it is normal to be *", "Do you enjoy being *"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 11:
-            reply_list = ["Did you come to me because you are *", "How long have you been *",
+            neutral_reply_list = ["Did you come to me because you are *", "How long have you been *",
                           "Do you believe it is normal to be *", "Do you enjoy being *"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 12:
-            reply_list = ["We were discussing you, not me.", "Oh, I *", "You're not really talking about me, are you?"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["We were discussing you, not me.", "Oh, I *", "You're not really talking about me, are you?"]
+            reply = helper(mood)
         elif rep_num == 13:
-            reply_list = ["What would it mean to you if you got *", "Why do you want *", "Suppose you got *",
+            neutral_reply_list = ["What would it mean to you if you got *", "Why do you want *", "Suppose you got *",
                           "What if you never got *", "I sometimes also want *"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 14:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 15:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 16:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 17:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 18:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 19:
-            reply_list = ["Why do you ask?", "Does that question interest you?",
+            neutral_reply_list = ["Why do you ask?", "Does that question interest you?",
                           "What answer would please you the most?", "What do you think?",
                           "Are such questions on your mind often?", "What is it that you really want to know?",
                           "Have you asked anyone else?", "Have you asked such questions before?",
                           "What else comes to your mind when you ask that?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 20:
             reply_list = ["Names don't interest me.", "I don't care about names.  Please go on."]
             reply = random.choice(reply_list)
         elif rep_num == 21:
-            reply_list = ["Is that the real reason?", "Don't any other reasons come to mind?",
+            neutral_reply_list = ["Is that the real reason?", "Don't any other reasons come to mind?",
                           "Does that reason explain anything else?", "What other reasons might there be?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 22:
-            reply_list = ["Please don't apologize!", "Apologies are not necessary."]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Please don't apologize!", "Apologies are not necessary."]
+            reply = helper(mood)
         elif rep_num == 23:
-            reply_list = ["What does that dream suggest to you?", "Do you dream often?",
+            neutral_reply_list = ["What does that dream suggest to you?", "Do you dream often?",
                           "What persons appear in your dreams?", "Are you disturbed by your dreams?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 24:
-            reply_list = ["How do you do.  Please state your problem."]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["How do you do.  Please state your problem."]
+            reply = helper(mood)
         elif rep_num == 25:
-            reply_list = ["How do you do.  Please state your problem."]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["How do you do.  Please state your problem."]
+            reply = helper(mood)
         elif rep_num == 26:
-            reply_list = ["You don't seem quite certain.", "Why the uncertain tone?", "Can't you be more positive?",
+            neutral_reply_list = ["You don't seem quite certain.", "Why the uncertain tone?", "Can't you be more positive?",
                           "You aren't sure?", "Don't you know?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 27:
-            reply_list = ["Are you saying no just to be negative?", "You are being a bit negative.", "Why not?",
+            neutral_reply_list = ["Are you saying no just to be negative?", "You are being a bit negative.", "Why not?",
                           "Are you sure?", "Why no?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 28:
-            reply_list = ["Why are you concerned about my *", "What about your own *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Why are you concerned about my *", "What about your own *"]
+            reply = helper(mood)
         elif rep_num == 29:
-            reply_list = ["Can you think of a specific example?", "When?", "What are you thinking of?",
+            neutral_reply_list = ["Can you think of a specific example?", "When?", "What are you thinking of?",
                           "Really, always?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 30:
-            reply_list = ["Do you really think so?", "But you are not sure you *", "Do you doubt *"]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["Do you really think so?", "But you are not sure you *", "Do you doubt *"]
+            reply = helper(mood)
         elif rep_num == 31:
-            reply_list = ["In what way?", "What resemblence do you see?", "What does the similarity suggest to you?",
+            neutral_reply_list = ["In what way?", "What resemblance do you see?", "What does the similarity suggest to you?",
                           "What other connections do you see?", "Could there really be some connection?", "How?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 32:
-            reply_list = ["You seem quite positive.", "Are you sure?", "I see.", "I understand."]
-            reply = random.choice(reply_list)
+            neutral_reply_list = ["You seem quite positive.", "Are you sure?", "I see.", "I understand."]
+            reply = helper(mood)
         elif rep_num == 33:
-            reply_list = ["Why do you bring up the topic of friends?", "Do your friends worry you?",
+            neutral_reply_list = ["Why do you bring up the topic of friends?", "Do your friends worry you?",
                           "Do your friends pick on you?", "Are you sure you have any friends?",
                           "Do you impose on your friends?", "Perhaps your love for your friends worries you."]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
         elif rep_num == 34:
-            reply_list = ["Do computers worry you?", "Are you frightened by machines?", "Why do you mention computers?",
+            neutral_reply_list = ["Do computers worry you?", "Are you frightened by machines?", "Why do you mention computers?",
                           "What do you think machines have to do with your problem?",
                           "Don't you think computers can help people?", "What is it about machines that worries you?"]
-            reply = random.choice(reply_list)
+            reply = helper(mood)
 
         return reply
-
 
     def buildreply(self, sentence: str):
         rh_sent = sentence.split()
         reply_num = int(rh_sent[0])
         rh_reply = self.list_to_string(rh_sent[1:])
-        lh_reply = self.getreply(reply_num)
+        lh_reply = self.getreply(reply_num, self.mood)
         full_reply = lh_reply
         if lh_reply[-1] == '*':
             full_reply = lh_reply[:-1] + rh_reply + "?"
